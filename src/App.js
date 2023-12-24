66ec4e26244127d80772f032a6736cf16f56379d
PreetAngadSingh <preetangad107@gmail.com>
2023-12-24 13:49:00 -0500

    rendering a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
+const Person = () => {
+  return (
+    <>
+      <h1>Name: John</h1>
+      <h1>Last Name: Doe</h1>
+      <h1>Age: 30</h1>
+    </>
+  )
+}
+
 const App = () => {
-  const name = "John";
+  const name = "Preet";
   const isNameShowing = false;
 
   return (
     <div className="App">
-      <h1> Hello, {isNameShowing ? name : 'someone'}</h1>
+      <Person/>
     </div>
   );
 }

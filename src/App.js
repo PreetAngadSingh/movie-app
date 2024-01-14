320b36c9b7db00981f8515a2dbb2e0d80ce4df5f
PreetAngadSingh <preetangad107@gmail.com>
2024-01-14 13:05:46 -0500

    full-appplication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
+import {useState, useEffect} from "react";
+import MovieCard from "./MovieCard";
+
 import './App.css';
-import {useState} from 'react';
+import SearchIcon from './search.svg'
+//8e050c99 - API Key
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=8e050c99";
 
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount+1)}>+</button>
-    </div>
-  );
+const movie1 = {
+    "Title": "Spiderman and Grandma",
+    "Year": "2009",
+    "imdbID": "tt1433184",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+}
+
+const App = () =>{
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchMovies = async(title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(()=>{
+        searchMovies('Spiderman');
+    },[]);
+
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e)=> setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={()=>searchMovies(searchTerm)}
+                />
+            </div>
+            {
+                movies?.length>0 ? (
+                    <div className="container">
+                        {movies.map((movie)=>(
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+            
+        </div>
+    );
 }
 
 export default App;
